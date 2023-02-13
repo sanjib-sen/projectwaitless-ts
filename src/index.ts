8a468b999b96842cdf49ee43e31d764bb89740a5
unknown <sksenonline@gmail.com>
2023-02-13 12:22:15 +0600

    Upgraded Structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { UseScraper } from "./scrape";
+import { UseScraper } from "./lib/scrape";
 
 async function main() {
   const scraper = new UseScraper("https://en.wikipedia.org/wiki/Prague");
--- src/lib/scrape.ts
+++ src/lib/scrape.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import cheerio from "cheerio";
-async function useRequests(url: string) {
+async function useRequests(url: string): Promise<cheerio.Selector> {
   let response: AxiosResponse;
   try {
     response = await axios.get(url, { params: {} });
@@ -12,14 +12,14 @@ async function useRequests(url: string) {
 }
 
 export class UseScraper {
-  element: string | cheerio.Root;
-  constructor(param: string | cheerio.Root) {
+  element: string | cheerio.Selector
+  constructor(param: string | cheerio.Selector) {
     this.element = param;
   }
-  async getSouped() {
+  async getSouped():Promise<cheerio.Selector> {
     if (typeof this.element === "string") {
       return await useRequests(this.element);
-    } else if (this.element instanceof cheerio.root) {
+    } else if (this.element instanceof cheerio) {
       return this.element;
     }
   }
@@ -27,5 +27,10 @@ export class UseScraper {
   async getTextById(id: string): Promise<string> {
     const $ = await this.getSouped();
     return $(`\#${id}`).text();
+  }
+
+  async getTextByTagNClass(tag:string, className: string, index:number=0): Promise<string> {
+    const $ = await this.getSouped();
+    return $(`${tag}.${className}`).text();
   }
 }
